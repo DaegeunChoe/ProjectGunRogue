a982665ae29bccc2781e7715dbf6c876a10e2d7b
khy1564 <comkhy@gmail.com>
2025-12-29 20:21:26 +0900

    Feature/weapon/option pool (#245)
    * 무기 기본 옵션 구현
    
    기본 옵션 종류
    - 공격력 증가
    - 약점 공격력 증가
    - 공격력 %증가
    - 약점 공격력 %증가
    - 장전 속도 % 증가
    - 연사 속도 % 증가
    - 탄창량 %증가
    
    * 적용 옵션들 위치 이동
    
    * 조건 옵션 기능 구현 - 1
    
    * Revert "조건 옵션 기능 구현 - 1"
    
    This reverts commit b9410c31c59f21b7e0107304d960f3c9f663f9b7.
    
    * 조건 옵션 기본 구현
    
    - 현재 체력이%이하면 공격력이 증가하는옵션 구현
    
    * 탄창 수에 따른 옵션 추가 및 적용되는 옵션들에 Tag로 관리하게 Tag 추가, UI에 옵션 내용 표시 내용 수정
    
    - 탄창 수에 따른 옵션 추가: 탄창 상황에 따라 적용되는 BaseMMC 구현
    - 적용되는 옵션들에 Tag로 관리하게 Tag 추가: 적용되는 옵션과 조건들을 Tag로 관리
    - UI에 옵션 내용 표시 내용 수정: Tag에 맞는 값들을 UI에 표시되게 수정
    
    * 옵션 값 수정
    
    * 옵션 텍스트 변경, 옵션 값 재조정(밸런싱), 퍼센테이지는 100을 곱해서 출력하도록 변경
    
    * 버그 수정
    [1] 재장전 속도와 공격 속도가 느려지던 버그 해결 (30%와 30% 증가를 구분. 30%증가는 130%)
    [2] 가지고 있지만, 사용하지 않는 무기의 옵션이 적용되던 버그 해결
    
    * 주석처리한 코드 제거
    
    * 옵션 값 수정
    
    * 클라이언트 옵션 텍스트 안되는 문제 해결
    
    TMap은 Replicated이 되지 않아 발생한 문제 TMap되신 TArray로 수정하여 문제 해결
    
    * 무기 기본 데미지 effect로 관리하게 구현
    
    * [1] 무기 강화 수치를 WeaponDamage_Base에 더하는 것이 아니라, Additive에 더하도록 수정 [2] 무기 UI에 올바른 공격력, 약점배율, 공격속도, 탄창 값이 들어가도록 변경
    
    ---------
    
    Co-authored-by: Daegeun Choe <asdf12gg@gmail.com>

--- Source/GunRogue/Weapon/Option/NormalOptions/GRReloadPercentUpEffect.h
+++ Source/GunRogue/Weapon/Option/NormalOptions/GRReloadPercentUpEffect.h
@@ -1,1 +1,17 @@
-﻿
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AbilitySystem/GRGameplayEffect.h"
+#include "GRReloadPercentUpEffect.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class GUNROGUE_API UGRReloadPercentUpEffect : public UGRGameplayEffect
+{
+	GENERATED_BODY()
+	
+public:
+	UGRReloadPercentUpEffect();
+};
